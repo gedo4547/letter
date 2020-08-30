c3625618076d0a79cf7e98559697103290688b80
管叶栋 <gedo4547@foxmail.com>
2020-08-30 17:22:32 +0800

    init

---
+++ src/Letter.IO/Scheduler/Work.cs
@@ -0,0 +1,14 @@
+﻿﻿namespace System.Threading
+{
+    readonly struct Work
+    {
+        public readonly Action<object> Callback;
+        public readonly object State;
+
+        public Work(Action<object> callback, object state)
+        {
+            Callback = callback;
+            State = state;
+        }
+    }
+}
---
+++ src/Letter.IO/Socket/SocketBase.cs
@@ -0,0 +1,20 @@
+﻿﻿using System.ComponentModel;
+using System.IO.Pipelines;
+
+namespace System.Net.Sockets
+{
+     [EditorBrowsable(EditorBrowsableState.Never)]
+    public abstract class SocketBase : IDisposable
+    {
+        protected readonly Socket _socket;
+        protected readonly SocketAwaitableEventArgs _awaitableEventArgs;
+
+        protected SocketBase(Socket socket, PipeScheduler scheduler)
+        {
+            _socket = socket;
+            _awaitableEventArgs = new SocketAwaitableEventArgs(scheduler);
+        }
+
+        public void Dispose() => _awaitableEventArgs.Dispose();
+    }
+}
