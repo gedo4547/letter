005229e98f39950fc3ea4ac55dec1e4a7bed278e
gyd <gedo4547@foxmail.com>
2020-08-31 16:06:15 +0800

    OrderConvertor

---
+++ src/Letter.IO/Binary/BinaryOrderConvertorFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace Letter.IO
+{
+    public static class BinaryOrderConvertorFactory
+    {
+        private static OrderConvertorBigEndianImpl bigEndian = new OrderConvertorBigEndianImpl();
+        private static OrderConvertorLittleEndianImpl littleEndian = new OrderConvertorLittleEndianImpl();
+        
+        public static IBinaryOrderConvertor GetConvertor(BinaryOrder order)
+        {
+            switch (order)
+            {
+                case BinaryOrder.BigEndian: return bigEndian;
+                case BinaryOrder.LittleEndian: return littleEndian;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/Letter.IO/Binary/IBinaryOrderConvertor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Letter.IO
+{
+    public interface IBinaryOrderConvertor
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        short ReadInt16(in ReadOnlySpan<byte> span);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        ushort ReadUInt16(in ReadOnlySpan<byte> span);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        int ReadInt32(in ReadOnlySpan<byte> span);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        uint ReadUInt32(in ReadOnlySpan<byte> span);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        long ReadInt64(in ReadOnlySpan<byte> span);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        ulong ReadUInt64(in ReadOnlySpan<byte> span);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void WriteInt16(in Span<byte> destination, ref short value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void WriteUInt16(in Span<byte> destination, ref ushort value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void WriteInt32(in Span<byte> destination, ref int value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void WriteUInt32(in Span<byte> destination, ref uint value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void WriteInt64(in Span<byte> destination, ref long value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void WriteUInt64(in Span<byte> destination, ref ulong value);
+    }
+}
